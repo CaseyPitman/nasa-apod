cb29ad7ff4bd23f737223e3f1e04360141502f6f
Casey The Great <casey@caseypitman.com>
2021-02-23 15:42:52 -0600

    Add api key to baseURL.

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 // TODO: add api key to baseURL;
 
+const apiKey = process.env.REACT_APP_NASA_KEY;
+
 export default axios.create({
-  baseURL: `https://api.nasa.gov/planetary/apod?api_key=`,
+  baseURL: `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`,
 });
