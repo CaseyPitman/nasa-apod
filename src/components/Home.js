179c6c12534796b512b20892f1080a44e37e617c
Casey The Great <casey@caseypitman.com>
2021-02-23 20:45:51 -0600

    Add quote and link to apod

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,25 @@
 
 import React from 'react';
 
+//components
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+// Styles
 import '../css/home.css';
 
 const Home = () => {
-  return <div className='home'>Home</div>;
+  return (
+    <div className='home'>
+      <h1>Somewhere, something incredible is waiting to be known.</h1>
+      <h1>- Carl Sagan</h1>
+      <Link to='apod'>
+        <Button variant='outline-info' size='lg'>
+          Blast Off
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
