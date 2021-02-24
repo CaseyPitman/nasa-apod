e7348596f00ce44506360faabbb0d5fc2cd2de11
Casey The Great <casey@caseypitman.com>
2021-02-24 13:44:32 -0600

    Add datepicker.

--- src/components/Apod.js
+++ src/components/Apod.js
@@ -1,13 +1,28 @@
 // Displays apod image for
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-//search for specific date
 //random pic?
 //display pic and info
 
+
+// TODO: limit datepicker to dates before 1995-06-16 (error handling for response?)
+
 const Apod = () => {
-   return <h1>APOD</h1>
-}
+  const [startDate, setStartDate] = useState(new Date());
+
+  return (
+    <div className='apod'>
+      <DatePicker
+        selected={startDate}
+        maxDate={new Date()}
+        onChange={date => setStartDate(date)}
+      />
+      {/* <h1>APOD</h1>; */}
+    </div>
+  );
+};
 
 export default Apod;
