99ff6650a557d2a78a9e535e4de3e45dccfcbcb0
Casey The Great <casey@caseypitman.com>
2021-02-25 15:20:38 -0600

    Add date formatting.

--- src/components/Apod.js
+++ src/components/Apod.js
@@ -7,6 +7,7 @@ import Button from 'react-bootstrap/Button';
 
 //Helper functions
 import getApod from '../axios/axios';
+import { format } from 'date-fns';
 
 //Styles
 import 'react-datepicker/dist/react-datepicker.css';
@@ -14,32 +15,31 @@ import 'react-datepicker/dist/react-datepicker.css';
 // import axios from './axios/axios'
 const apiKey = process.env.REACT_APP_NASA_KEY;
 
-
 const Apod = () => {
   const [startDate, setStartDate] = useState(new Date());
-  const [apodData, setApodData] = useState({})
+  const [apodData, setApodData] = useState({});
   useEffect(() => {
     getData();
   }, []);
 
-  const getData = async () => {
-    // TODO: ternary after apiKey for search or random. 
+  const getData = async query => {
+    // TODO: ternary after apiKey for search or random.
     const response = await getApod.get(`/apod?api_key=${apiKey}`);
     console.log(response.data);
-    setApodData(response.data)
+    setApodData(response.data);
   };
 
   // TODO: ternary to determine between image and video.
 
-
   const handleSelectDate = date => {
-    console.log(`You selected the date: ${date}`);
-    // TODO: call for getData w/ search parameters
+     const formattedDate = format(new Date(date), 'yyyy-MM-dd')
 
+    console.log(`You selected the date: ${formattedDate}`);
+
+    // TODO: call for getData w/ search parameters - YYYY-MM-DD
   };
 
-
-  // TODO: call for data, random pic. 
+  // TODO: call for data, random pic.
 
   return (
     <div className='apod'>
@@ -49,13 +49,14 @@ const Apod = () => {
         onChange={date => setStartDate(date)}
         minDate={new Date(1995, 6, 20)}
         onSelect={handleSelectDate(startDate)}
+    
       />
       <Button variant='outline-success'>Random Date</Button>
-      <div>{apodData.date}</div>
+      <div>{format(new Date(apodData.date), 'MMMM dd, yyyy')}</div>
       <div>{apodData.title}</div>
 
       <div>
-        <img src = {apodData.url} alt = {apodData.title} />
+        <img src={apodData.url} alt={apodData.title} />
       </div>
       <div>{apodData.explanation}</div>
     </div>
