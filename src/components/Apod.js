09f8771a4a871d95074aea524973f543750265e0
Casey The Great <casey@caseypitman.com>
2021-02-25 14:56:08 -0600

    ADD render of date, title, image and explanation - no styles

--- src/components/Apod.js
+++ src/components/Apod.js
@@ -14,25 +14,32 @@ import 'react-datepicker/dist/react-datepicker.css';
 // import axios from './axios/axios'
 const apiKey = process.env.REACT_APP_NASA_KEY;
 
-//random pic? button
-//display searched pic
-//display pic and info
 
 const Apod = () => {
   const [startDate, setStartDate] = useState(new Date());
-
+  const [apodData, setApodData] = useState({})
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
+    // TODO: ternary after apiKey for search or random. 
     const response = await getApod.get(`/apod?api_key=${apiKey}`);
     console.log(response.data);
+    setApodData(response.data)
   };
+
+  // TODO: ternary to determine between image and video.
+
 
   const handleSelectDate = date => {
     console.log(`You selected the date: ${date}`);
+    // TODO: call for getData w/ search parameters
+
   };
+
+
+  // TODO: call for data, random pic. 
 
   return (
     <div className='apod'>
@@ -44,6 +51,13 @@ const Apod = () => {
         onSelect={handleSelectDate(startDate)}
       />
       <Button variant='outline-success'>Random Date</Button>
+      <div>{apodData.date}</div>
+      <div>{apodData.title}</div>
+
+      <div>
+        <img src = {apodData.url} alt = {apodData.title} />
+      </div>
+      <div>{apodData.explanation}</div>
     </div>
   );
 };
