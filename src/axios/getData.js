5d4e415dbd2be5bf1e1766a01441c43e7e8ec880
Casey The Great <casey@caseypitman.com>
2021-02-26 19:23:35 -0600

    FIX: retrieve and display APOD on load.

--- src/axios/getData.js
+++ src/axios/getData.js
@@ -4,7 +4,7 @@ const apiKey = process.env.REACT_APP_NASA_KEY;
 const getData = async (query = {}) => {
   try {
     const response = await getApod.get(`/apod?api_key=${apiKey}`, query);
-    console.log(response.data);
+    return response.data;
   } catch (err) {
     console.log(`error: ${err}`);
   }
--- src/components/Apod.js
+++ src/components/Apod.js
@@ -13,32 +13,33 @@ import dayjs from 'dayjs';
 //Styles
 import 'react-datepicker/dist/react-datepicker.css';
 
-
 const Apod = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [apodData, setApodData] = useState({});
 
   useEffect(() => {
-
-    getData();
+    const fetch = async () => {
+      const result = await getData();
+      console.log(result)
+      setApodData(result);
+    };
+    fetch();
   }, []);
-
-  
 
   // TODO: ternary to determine between image and video.
 
   const handleSelectDate = date => {
     const formattedDate = format(new Date(date), 'yyyy-MM-dd');
     console.log(`You selected the date: ${formattedDate}`);
-    const query = `&date=${formattedDate}`;
-    getData(query, 'date');
+    // const query = `&date=${formattedDate}`;
+    // getData(query, 'date');
   };
 
   const handleRandom = () => {
     console.log('you want a random pic');
     //TODO: query will contain count = 1
-    const query = `&count=1`;
-    getData(query, 'random');
+    // const query = `&count=1`;
+    // getData(query, 'random');
   };
 
   const displayDate = () => {
@@ -49,7 +50,6 @@ const Apod = () => {
     return formattedDate;
   };
 
-  // TODO: call for data, random pic.
   const renderMedia = () => {
     if (!apodData) {
       return <div></div>;
@@ -65,13 +65,25 @@ const Apod = () => {
     }
   };
 
-/* 
+  const renderApod = () => {
+    if (!apodData) {
+      return <div></div>;
+    } else {
+      return (
+        <div>
+          <div>{apodData.title}</div>
+          <div>{renderMedia()}</div>
+          <div>{apodData.explanation}</div>
+        </div>
+      );
+    }
+  };
+
+  /* 
 Try this:  
 Move getData to helper func. in the component
 have event funcs for selecting a date -onSelect (and this should call by default on first load, but maybe not, in which case go with useEffect) and random. In those event funcs do all the prep. format any dates for the call and assemble a params object. Pass that to the getData helper func. save the return to a variable and then manipulate it as needed - format, set the new startDate to properly fill in the serch field, whatever - before setting it as the apodData state. This should trigger the render. In theory. I hope. 
 */
-
-
 
   return (
     <div className='apod'>
@@ -89,10 +101,7 @@ have event funcs for selecting a date -onSelect (and this should call by default
       <Button variant='outline-info'>Search NASA Image Library</Button>
 
       <div>{displayDate()}</div>
-      <div>{apodData.title}</div>
-
-      <div>{renderMedia()}</div>
-      <div>{apodData.explanation}</div>
+      <div>{renderApod()}</div>
     </div>
   );
 };
