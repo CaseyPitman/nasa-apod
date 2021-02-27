9efe3cb33009800d2f55dbcf8c0fb61f5141b6ed
Casey The Great <casey@caseypitman.com>
2021-02-27 11:45:14 -0600

    ADD handleSubmit, CONTROL input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 // This component will allow users to search the NASA image library.
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 //Components
@@ -9,18 +9,27 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(`submit: ${searchTerm}`);
+  };
+
   return (
     <div>
       <h1>Search Page</h1>
       <Link to='/apod'>
         <Button variant='outline-info'>Back to Picture of the Day</Button>
       </Link>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <InputGroup>
           <Form.Control
             placeholder='Saturn'
             aria-label='Search Term'
             aria-describedby='basic-addon2'
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
           />
           <InputGroup.Append>
             <Button variant='outline-success' type='submit'>
