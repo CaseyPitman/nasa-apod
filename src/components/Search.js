eea3809e29144239712c9b2039214b171bd5c804
Casey The Great <casey@caseypitman.com>
2021-02-27 11:29:04 -0600

    ADD button link to apod

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,21 @@
 // This component will allow users to search the NASA image library.
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+//Components
+import Button from 'react-bootstrap/Button';
 
 const Search = () => {
-  return <h1>Search Page</h1>;
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <Link to='/apod'>
+        <Button variant='outline-info'>Back to Picture of the Day</Button>
+      </Link>
+      
+    </div>
+  );
 };
 
 export default Search;
