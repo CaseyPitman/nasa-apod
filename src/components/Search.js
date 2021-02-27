ccfa5c893404d55d73f8d10a603b07a13a4e4592
Casey The Great <casey@caseypitman.com>
2021-02-27 11:35:46 -0600

    ADD form for search

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 
 //Components
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const Search = () => {
   return (
@@ -13,7 +15,20 @@ const Search = () => {
       <Link to='/apod'>
         <Button variant='outline-info'>Back to Picture of the Day</Button>
       </Link>
-      
+      <Form>
+        <InputGroup>
+          <Form.Control
+            placeholder='Saturn'
+            aria-label='Search Term'
+            aria-describedby='basic-addon2'
+          />
+          <InputGroup.Append>
+            <Button variant='outline-success' type='submit'>
+              Search
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </Form>
     </div>
   );
 };
