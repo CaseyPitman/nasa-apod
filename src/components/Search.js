2a0786ec9fc1d95fe4b454db7fe80e46c1e36e0a
Casey The Great <casey@caseypitman.com>
2021-03-05 14:21:23 -0600

    REFACTOR api call out of handle submit into discrete func for reuse

--- src/components/Search.js
+++ src/components/Search.js
@@ -21,7 +21,7 @@ Modal.setAppElement(`#root`);
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchPage, setSearchPage] = useState(1);
+  const [searchPage, setSearchPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
   const [totalHits, setTotalHits] = useState(0);
   const [searchResults, setSearchResults] = useState([]);
@@ -34,8 +34,7 @@ const Search = () => {
     history.push('/search/');
   }, [history]);
 
-  const handleSubmit = async e => {
-    e.preventDefault();
+  const getResults = async () => {
     const query = {
       params: {
         q: searchTerm,
@@ -44,24 +43,37 @@ const Search = () => {
       },
     };
     const result = await fetchSearch(query);
-    console.log(result);
-<<<<<<< HEAD
+    return result;
+  };
 
-=======
->>>>>>> pagination
-    history.push(`/search/${searchTerm}/page=${searchPage}`);
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setSearchPage(1);
+    const result = await getResults();
+    // const query = {
+    //   params: {
+    //     q: searchTerm,
+    //     media_type: 'image',
+    //     page: searchPage,
+    //   },
+    // };
+    // const result = await fetchSearch(query);
+    // console.log(result);
     setSearchResults(result);
-
+    history.push(`/search/${searchTerm}/page=${searchPage}`);
     setTotalHits(result.metadata.total_hits);
     setTotalPages(Math.ceil(result.metadata.total_hits / 100));
   };
 
-  const changePage = dir => {
-    console.log(`you wish to go to ${dir} page`);
-    //set cur page
-    //make call for next or previous
-    //set results to re-render
-  };
+  // const changePage = (e, dir) => {
+  //   console.log(`you wish to go to ${dir} page`);
+  //   //set cur page
+  //   const newSearchPage = searchPage + 1;
+  //   setSearchPage(newSearchPage);
+  //   handleSubmit(e);
+  //   //make call for next or previous
+  //   //set results to re-render
+  // };
 
   //TODO: enable pagination controls
   //TODO: add popover w/ title for each image.
@@ -101,11 +113,11 @@ const Search = () => {
 
     return (
       <Pagination className='pagination'>
-        <Pagination.Prev onClick={() => changePage('prev')} />
+        <Pagination.Prev onClick={e => handleSubmit(e, 'prev')} />
         <p className='pagination-page-count'>
           Page {searchPage} of {totalPages}
         </p>
-        <Pagination.Next onClick={() => changePage('next')} />
+        <Pagination.Next onClick={e => handleSubmit(e, 'next')} />
       </Pagination>
     );
   };
@@ -158,22 +170,6 @@ const Search = () => {
     );
   };
 
-<<<<<<< HEAD
-  //TODO: dynamically render pagination based on number of results. Complex w/ ellipses?
-  const renderPagination = () => {
-    const pages = [];
-    for (let i = 1; i < 5; i++) {
-      pages.push(
-        <Pagination.Item key={i} active={searchPage === i}>
-          {i}
-        </Pagination.Item>
-      );
-    }
-    return pages;
-  };
-
-=======
->>>>>>> pagination
   return (
     <div>
       <h1>Search Page</h1>
@@ -199,20 +195,7 @@ const Search = () => {
           </InputGroup.Append>
         </InputGroup>
       </Form>
-<<<<<<< HEAD
-      <div className='page-buttons'>
-        <Pagination>
-          {' '}
-          <Pagination.First />
-          <Pagination.Prev />
-          {renderPagination()}
-          <Pagination.Next />
-          <Pagination.Last />
-        </Pagination>
-      </div>
-=======
       {renderPagination()}
->>>>>>> pagination
       <div className='search-results grid-container'>{renderResults()}</div>
       {renderPagination()}
       <Modal
