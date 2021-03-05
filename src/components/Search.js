e324c43a4801830111737d6dadb58f9dead4ca37
Casey The Great <casey@caseypitman.com>
2021-03-05 15:28:22 -0600

    REFACTOR handleSubmit & changePage

--- src/components/Search.js
+++ src/components/Search.js
@@ -21,7 +21,7 @@ Modal.setAppElement(`#root`);
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchPage, setSearchPage] = useState(0);
+  const [searchPage, setSearchPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [totalHits, setTotalHits] = useState(0);
   const [searchResults, setSearchResults] = useState([]);
@@ -34,7 +34,34 @@ const Search = () => {
     history.push('/search/');
   }, [history]);
 
-  const getResults = async () => {
+  //FIXME: this method creates infinte api calls
+  // useEffect(() => {
+  //   if (searchPage === 0) {
+  //     return;
+  //   }
+  //   const getResults = async () => {
+  //     const query = {
+  //       params: {
+  //         q: searchTerm,
+  //         media_type: 'image',
+  //         page: searchPage,
+  //       },
+  //     };
+  //     const result = await fetchSearch(query);
+  //     console.log(result);
+  //     setSearchResults(result);
+  //     history.push(`/search/${searchTerm}/page=${searchPage}`);
+  //     setTotalHits(result.metadata.total_hits);
+  //     setTotalPages(Math.ceil(result.metadata.total_hits / 100));
+  //     return;
+  //   };
+  //   getResults();
+  // });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setSearchPage(1);
+
     const query = {
       params: {
         q: searchTerm,
@@ -43,43 +70,34 @@ const Search = () => {
       },
     };
     const result = await fetchSearch(query);
-    return result;
-  };
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    setSearchPage(1);
-    const result = await getResults();
-    // const query = {
-    //   params: {
-    //     q: searchTerm,
-    //     media_type: 'image',
-    //     page: searchPage,
-    //   },
-    // };
-    // const result = await fetchSearch(query);
-    // console.log(result);
+    console.log(result);
     setSearchResults(result);
     history.push(`/search/${searchTerm}/page=${searchPage}`);
     setTotalHits(result.metadata.total_hits);
     setTotalPages(Math.ceil(result.metadata.total_hits / 100));
   };
 
-  // const changePage = (e, dir) => {
-  //   console.log(`you wish to go to ${dir} page`);
-  //   //set cur page
-  //   const newSearchPage = searchPage + 1;
-  //   setSearchPage(newSearchPage);
-  //   handleSubmit(e);
-  //   //make call for next or previous
-  //   //set results to re-render
-  // };
+
+
+  const changePage = async dir => {
+    console.log(`you wish to go to ${dir} page`);
+    //set cur page
+    const newSearchPage = searchPage + 1;
+   
+    
+ 
+
+    console.log(searchPage);
+
+
+    //make call for next or previous
+    //set results to re-render
+  };
 
   //TODO: enable pagination controls
   //TODO: add popover w/ title for each image.
   //FIXME: when closing modal, don't reset to top of page.
-  //FIXME: when reloading page reset url.
-  //FIXME: pagination should only show when results exist.
+  //FIXME: set page to 1 on load and set to 1 at the begin of every search BEFORE the callback for the data Runs - useEffect to run as soon as searchPage changes?
 
   const renderResults = () => {
     if (searchResults.length === 0) {
@@ -113,11 +131,11 @@ const Search = () => {
 
     return (
       <Pagination className='pagination'>
-        <Pagination.Prev onClick={e => handleSubmit(e, 'prev')} />
+        <Pagination.Prev onClick={() => changePage('prev')} />
         <p className='pagination-page-count'>
           Page {searchPage} of {totalPages}
         </p>
-        <Pagination.Next onClick={e => handleSubmit(e, 'next')} />
+        <Pagination.Next onClick={() => changePage('next')} />
       </Pagination>
     );
   };
@@ -183,7 +201,9 @@ const Search = () => {
             aria-label='Search Term'
             aria-describedby='basic-addon2'
             value={searchTerm}
-            onChange={e => setSearchTerm(e.target.value)}
+            onChange={e => {
+              setSearchTerm(e.target.value);
+            }}
           />
           <InputGroup.Append>
             <Button
