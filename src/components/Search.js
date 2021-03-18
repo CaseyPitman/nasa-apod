bceeff83a89922820805d5ec37d527f9ff36d5f7
Casey The Great <casey@caseypitman.com>
2021-03-18 15:06:58 -0500

    Ship

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,7 +13,6 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Pagination from 'react-bootstrap/Pagination';
-import PageItem from 'react-bootstrap/PageItem';
 import Error from './Error';
 
 //styles
